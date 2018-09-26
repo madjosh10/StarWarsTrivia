90b755fee5902dc51caf53f762595d0e45af7a8b
madjosh10 <madrigaljosh10@yahoo.com>
2018-09-26 11:19:01 -0500

    Initial Commit

---
+++ StarWarsTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsTrivia
+//
+//  Created by Joshua Madrigal on 9/26/18.
+//  Copyright © 2018 Joshua Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
