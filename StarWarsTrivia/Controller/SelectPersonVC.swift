5916a36fa6696932e6b0bdafb592b8f825fb3a90
madjosh10 <madrigaljosh10@yahoo.com>
2018-09-26 11:45:37 -0500

    Added stack views with a stack view, and embedded a view on the parent stack view

--- StarWarsTrivia/Controller/SelectPersonVC.swift
+++ StarWarsTrivia/Controller/SelectPersonVC.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stackBg.layer.cornerRadius = 10
     }
 
 
